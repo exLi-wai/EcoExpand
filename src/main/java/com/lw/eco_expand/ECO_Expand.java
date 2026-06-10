50595d94d2de80f81eb368f78d1e1fc4dfec3136
li_waiwai <2157995671a@gmail.com>
2026-06-11 03:49:00 +0800

    2

--- src/main/java/com/lw/eco_expand/ECO_Expand.java
+++ src/main/java/com/lw/eco_expand/ECO_Expand.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class EcoExpand {
+public class ECO_Expand {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
